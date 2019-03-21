11e3b63e2859562d2cd185cded86c793ba2644f5
Mohit Choudhary <mohit.choudhary@scripbox.com>
2019-03-21 11:31:10 +0530

    add install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+wget https://raw.githubusercontent.com/ms-choudhary/cachr/master/cache_run -O /usr/bin/cache_run
+wget https://raw.githubusercontent.com/ms-choudhary/cachr/master/cache_get -O /usr/bin/cache_get
+wget https://github.com/ms-choudhary/cachr/releases/download/v1.0/cachr -O /usr/bin/cachr
+chmod +x /usr/bin/cache_run /usr/bin/cache_get /usr/bin/cachr
