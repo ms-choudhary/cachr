7f783c091134995aa3bb742b91be9c97d305d74e
Mohit Choudhary <mohit.choudhary@scripbox.com>
2019-03-21 17:02:29 +0530

    exit on error in scripts

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+set -o errexit
+
 wget https://raw.githubusercontent.com/ms-choudhary/cachr/master/cache_run -O /usr/bin/cache_run
 wget https://raw.githubusercontent.com/ms-choudhary/cachr/master/cache_get -O /usr/bin/cache_get
 wget https://github.com/ms-choudhary/cachr/releases/download/v1.0/cachr -O /usr/bin/cachr
