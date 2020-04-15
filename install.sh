637572e664c43ec985de5c61f17dec47a773c349
Vasu Adari <vasuakeel@gmail.com>
2020-04-16 03:34:20 +0530

    Replace github url to use Scripbox repo

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ if ! which git || ! which wget; then
   fi
 fi
 
-wget https://raw.githubusercontent.com/ms-choudhary/cachr/master/cache_run -O /usr/bin/cache_run
-wget https://raw.githubusercontent.com/ms-choudhary/cachr/master/cache_get -O /usr/bin/cache_get
-wget https://github.com/ms-choudhary/cachr/releases/download/v1.0/cachr -O /usr/bin/cachr
+wget https://raw.githubusercontent.com/scripbox/cachr/master/cache_run -O /usr/bin/cache_run
+wget https://raw.githubusercontent.com/scripbox/cachr/master/cache_get -O /usr/bin/cache_get
+wget https://github.com/scripbox/cachr/releases/download/v1.1/cachr -O /usr/bin/cachr
 chmod +x /usr/bin/cache_run /usr/bin/cache_get /usr/bin/cachr
